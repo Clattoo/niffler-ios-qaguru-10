8c1844f2cffe193d173734b6410da52901ae2da6
mashlemin <maksim.shl@neo.gg>
2026-02-11 21:27:30 +0300

    HW 18.3

--- Niffler/NifflerUITests/Test/SpendsUITests.swift
+++ Niffler/NifflerUITests/Test/SpendsUITests.swift
@@ -2,6 +2,10 @@ import XCTest
 
 final class SpendsUITests: TestCase {
     
+    lazy var descriptionTitle = newSpendPage.makeRandomSpendDescriptionTitle()
+    lazy var categoryTitle = newSpendPage.makeRandomSpendCategoryTitle()
+    let amount = "25"
+    
     func test_whenAddSpent_shouldShowSpendInList() {
         launchAppWithoutLogin()
         
@@ -22,7 +26,27 @@ final class SpendsUITests: TestCase {
         spendsPage
             .assertNewSpendIsShown(title: title)
     }
+    
+    func test_checkCategoryExistForUser() {
+        launchAppWithoutLogin()
+        
+        // Arrange
+        loginPage.input(login: "stage", password: "12345")
+        
+        if spendsPage.hasSpends() {
+            spendsPage.addSpent()
+            newSpendPage.inputSpent(title: descriptionTitle)
+        } else {
+            spendsPage.addSpent()
+            newSpendPage.createNewSpendAndCategory(amount: amount, title: descriptionTitle, categoryTitle: categoryTitle)
+        }
+        
+        // Assert
+        spendsPage.assertNewSpendIsShown(title: descriptionTitle)
+    }
 }
+
+
 
 extension UUID {
     static var randomPart: String {
